f6898fe809a404570e000dd625e8bc9f9a60c762
Inna949Festchuk <innabori@mail.ru>
2022-12-01 05:01:17 +0200

    1.0.2

--- Project_VK_to_Yandex.py
+++ Project_VK_to_Yandex.py
@@ -5,7 +5,6 @@ import time
 import requests
 from pprint import pprint
 
-
 class VkFoto:
     url = 'https://api.vk.com/method/photos.get'
     def __init__(self, token_vk, version='5.131'):
@@ -13,63 +12,46 @@ class VkFoto:
             'access_token': token_vk,
             'v': version    
         }
-
-    def profile_fotos(self, owner_id=764412154):
+    def profile_fotos(self, owner_id=457689717):
         '''
         owner_id — идентификационный номер владельца аккаунта ВКонтакте
         amount_foto  - количество выгружаемых фото максимального размера
         '''
         profile_fotos_params = {
+            'owner_id': owner_id,
             'album_id': 'profile',
-            'rev': '0',
-            'extended': '1',
-            'photo_sizes': '1'
+            'extended': True,
+            'photo_sizes': True,
+            'count': 1000
         }
-        req = requests.get(self.url, params={**self.params, **profile_fotos_params}).json()
-        return req['response']['items'][0]['sizes']
-        
+        response = requests.get(self.url, params={**self.params, **profile_fotos_params}).json()
+        return response
+
 vk_foto_1 = VkFoto(token_vk, '5.131')
-dicts_foto = vk_foto_1.profile_fotos()
-# pprint(dicts_foto)
+count_z = vk_foto_1.profile_fotos()['response']['count']
 
-# i = 0
-# for dict_foto in dicts_foto:
-#     size_foto = dict_foto['height'] * dict_foto['width']
-# #     size_dict = {size_foto:dicts_foto[i]} # Делаем новый словарь с ключом - размер фото 
-# #     # lst.append(list(size_dict.items())) # Делаем из нового словаря список и ...
-# #     sorted_size_list = dict(sorted((size_dict.items()), reverse=True)) # ... сортируем этот список по размеру фото и по убывагнию
-    
-# #     i += 1
-# # pprint(size_dict)
-# # pprint(sorted_size_list[0:4]) #Выбираем с последние четыре фото для сохранения на яндекс диске
+for i in range(int(count_z)):
+    print(f'Число фотографийв профиле: {count_z}, это {i + 1} фотография.')
+    # Получаем контент с сылкой на фото
+    dicts_foto_1 = vk_foto_1.profile_fotos()['response']['items'][i]['sizes']
+    # pprint (dicts_foto_1)
+    # # Получаем дату добавления фото в профиль
+    # date_foto_1 = vk_foto_1.profile_fotos()['response']['items'][i]['date']
+    # pprint(date_foto_1)
+    # Получаем количество лайкосов фотки профиля
+    # likes_foto_1 = vk_foto_1.profile_fotos()['response']['items'][i]['likes']['count']
+    # pprint(likes_foto_1)
 
-# lst = [sorted({dict_foto['height'] * dict_foto['width']:dict_foto}.items()) for dict_foto in dicts_foto]
+    # Dictionary Comprehension (словарное включение)
+    ''' Делаем новый словарь с ключом - размер фото 
+        Делаем из нового словаря список и ...
+        сортируем этот список по размеру фото и по убыванию
+        Выбираем три ссылки на фото для сохранения на яндекс '''
+    dct = {v['height'] * v['width']:v['url'] for v in dicts_foto_1}
+    sort_dct = dict(sorted(dct.items(), reverse=True)[0:2])
+    pprint (sort_dct)
 
-# pprint (sorted(lst))
-
-dct = {v['height'] * v['width']:v for v in dicts_foto}
-sort_dct = dict(sorted(dct.items(), reverse=True)[0:4])
-
-pprint (sort_dct)
-
-# lst = []
-# for dict_foto in dicts_foto:
-#     size_foto = dict_foto['height'] * dict_foto['width']
-#     size_dict = {size_foto:list(dict_foto.items())} # Делаем новый словарь с ключом - размер фото 
-#     lst.append(list(size_dict.items())) # Делаем из нового словаря список и ...
-#     sorted_size_list = sorted(lst, reverse=True) # ... сортируем этот список по размеру фото и по убывагнию
-# pprint(sorted_size_list[0:4]) #Выбираем с последние четыре фото для сохранения на яндекс диске
-
-
-
-# # Получаем ссылку на фото профиля
-# pprint(vk_foto_1.profile_fotos()[0]['sizes'][0]['url'])
-# # Получаем размер фото
-# height = vk_foto_1.profile_fotos()[0]['sizes'][0]['height']
-# width = vk_foto_1.profile_fotos()[0]['sizes'][0]['width']
-# pprint(f'{height}x{width} px')
-
-# # Получаем дату добавления фото в профиль
-# pprint(vk_foto_1.profile_fotos()[0]['date'])
-# # Получаем количество лайкосов фотки профиля
-# pprint(vk_foto_1.profile_fotos()[0]['likes']['count'])
+import json
+with open ('metadates.json', 'w') as outfile:
+    js = [{ "file_name": "34.jpg", "size": "z"}]
+    json.dump(js, outfile, indent=2)
